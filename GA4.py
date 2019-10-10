c9f95fc7b4aa0e2314b5108c1d646017d3650b14
Hideki <leoesaki@gmail.com>
2019-10-10 16:31:52 +0900

    comments added

--- GA4.py
+++ GA4.py
@@ -22,7 +22,7 @@ import numpy as np
 import random
 
 #random.seed(a=None, version=2)
-random.seed(1)
+#random.seed(1)
 
 #constant
 EupThreshold = 25 #Max energy - Eharvest
@@ -35,51 +35,58 @@ EnergyAvailablePercentage = 60
 
 
 class MoniModel(Model):
-    
+    """
+    A model for monitoring agents
+    """
     def __init__(self, N, width, height):
-        self.num_agents = N
+        
+        
+        #Monitoring space 
         self.width = width
         self.height = height
         self.grid = MultiGrid(height, width, False) #non toroidal grid
+                
         self.schedule = SimultaneousActivation(self)
         
         self.abCount = 0 #initial abnormality count
         self.detectedAb = 0
-        # Create agents
-        self.coveredArea = []
         self.interactionCount = 0
-        self.interactionRateAverage = 0
-        self.coveragePercentage = 0
-        self.coveragePercentageAverage = 0
-        
-        
+# =============================================================================
+#         self.coveredArea = []
+
+#         self.interactionRateAverage = 0
+#         self.coveragePercentage = 0
+#         self.coveragePercentageAverage = 0
+# =============================================================================
+        # Create agents
+       
+        self.num_agents = N
         for i in range(self.num_agents):
-        
             x = floor(self.width/N*i+self.width/N/2)
-#            create and add agent with id number i to the scheduler
+#           create and add agent with id number i to the scheduler
             a = MoniAgent(i, self)
             self.schedule.add(a)
-            
             #place agent at the center of its limit coor
             self.grid.place_agent(a, (x, 0))
            
 #        this part is for visualization only
-        self.running = True
+#        self.running = True
 
     def step(self):
-        self.interactionCount = 0
+#        self.interactionCount = 0
         self.schedule.step()
-        
-    
         
 
     def run_model(self, n):
         for i in range(n):
-#            self.initPos()
+#           self.initPos()
             self.step()
-#            print(self.schedule.steps)
-            
-    def fitness(self):
+#           print(self.schedule.steps)
+    
+    """
+    Calculate fitness of a swarm (success ratio)
+    """
+    def fitness(self): 
         for _ in range(100):
             self.step()
         return self.detectedAb/self.abCount
@@ -89,27 +96,17 @@ class MoniAgent(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.nextPos = (0,0) # careful ----------------------------------------
-        self.sensitivity = 10
+        self.sensitivity = 5
+        
         self.genome = np.random.randint(1,10,10)
         
-        self.energy = 25 #initial energy
-#        self.Ereport = 15 #energy consumed if the agent communicates with other
-#        self.charging = 0 #charging flag
-#
-#        self.spanningTree = []
-#        self.oldCellList = [(-1,-1)] #dummy first value
-        
-        #be careful with empty list
-#        self.previousCell = self.pos
-        
+        self.energy = 25 #initial energy        
         self.regionWidth = ceil(self.model.width/self.model.num_agents) #width of the region to be covered by this agent
         self.originX = floor(self.model.width/self.model.num_agents*unique_id+self.model.width/self.model.num_agents/2)
         self.origin = (self.originX,0)
         
-        self.forward = 1 #direction along the spanning tree
         self.commuRange = self.model.width*2/self.model.num_agents
-        self.updateRegion = 1 #monitoring region will always  be updated in the first run
-     
+#        self.updateRegion = 1 #monitoring region will always  be updated in the first run
 #        self.region = [floor(self.model.width/self.model.num_agents*unique_id),floor(self.model.width/self.model.num_agents*(unique_id+1))-1] #range of region of each agent
     
     def confrontOther(self):
@@ -118,8 +115,7 @@ class MoniAgent(Agent):
             if self != agent:
                 if abs(self.pos[0]-agent.pos[0])+abs(self.pos[1]-agent.pos[1]) < 3:
                     self.model.interactionCount += 1
-                    return True
-                
+                    return True    
         return False
         
     def distance(self,pos):
@@ -163,7 +159,7 @@ class MoniAgent(Agent):
                  self.sensitivity -= 0.1
                 
         if (self.sensitivity > self.genome[self.model.schedule.steps%10]):    
-            if random.random() > 0.8:
+            if random.random() > 0.8: #probability to encounter an abnormality
                 self.model.abCount += 1
                 if self.energy > Ereport:
                     self.energy -= Ereport
@@ -184,72 +180,18 @@ class MoniAgent(Agent):
 # =============================================================================
 #        print(self.pos)
 #        print(self.model.schedule.steps)
-       
-        
 # =============================================================================
-# 
-# 
-# 
-# #visualization module =======================================================================
-# #============================================================================================
-#             
-# from mesa.visualization.ModularVisualization import ModularServer
-# from mesa.visualization.modules import CanvasGrid
-# from mesa.visualization.modules import ChartModule
-# from mesa.visualization.UserParam import UserSettableParameter
-# 
-# 
-# def agent_portrayal(agent):
-#     portrayal = {"Shape": "circle",
-#                  "Filled": "true",
-#                  "r": 0.5}
-# 
-#     if 1:
-#         portrayal["Color"] = "green"
-#         portrayal["Layer"] = 0
-#     else:
-#         portrayal["Color"] = "red"
-#         portrayal["Layer"] = 0
-#         portrayal["r"] = 0.5
-#     return portrayal
-# 
-# 
-# grid = CanvasGrid(agent_portrayal, 15, 15, 512, 512)
-# 
-# 
-# 
-#     
-# model_params = {
-#     "N": UserSettableParameter('slider', "Number of agents", 2, 1, 200, 1,
-#                                description="Choose how many agents to include in the model"),
-#     "width": 15, 
-#     "height": 15
-# }
-# #server = ModularServer(MoniModel, [grid, chart], "Money Model", model_params)
-# server = ModularServer(MoniModel, [grid], "Monitoring pattern", model_params)
-# server.port = 8433
-# server.launch()
-# 
-# # =============================================================================
-# # 
-# # # For testing on Jupyter Notebook
-# # model = MoniModel(1, 20, 20)
-# # for i in range(90):
-# #     model.step()
-# # =============================================================================
-# 
-# =============================================================================
+
 
 modelList = []
 
-generationCount = 30
+generationCount = 100
 genomeLength = 10
 swarmSize = 10 #number of individuals in each swarm
 swarmPopulation = 10 #number of swarms
 mirrorList = [[[0 for x in range(genomeLength)]for y in range(swarmSize)] for z in range(swarmPopulation)]
 
 for i in range(swarmPopulation):
-#    print(i)
     model = MoniModel(swarmSize,10,10)
     for agent in model.schedule.agents:
 #        agent.genome = [random.uniform(0,10) for _ in range(genomeLength)] #float random
@@ -345,8 +287,8 @@ for i in range(generationCount):
     b = sorted(range(len(a)), key=lambda k: a[k], reverse = True)
     print(b)
 
-flatten_list = [j for sub in mirrorList[b[0]] for j in sub]
-plt.hist(flatten_list,bins = 10)
+flat = [j for sub in mirrorList[b[0]] for j in sub]
+plt.hist(flat,bins = 10)
 # =============================================================================
 # for i in range(generationCount):
 #     for j in range(swarmPopulation):
